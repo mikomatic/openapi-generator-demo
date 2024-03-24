163757081283662fa558a8f1772168b1ef17260e
Miguel Ortega <miguel.ortega84@gmail.com>
2024-03-24 19:41:17 +0100

    initial commit

---
+++ api-producer/src/main/java/com/example/api/producer/ApiProducerApplication.java
@@ -0,0 +1,12 @@
+package com.example.api.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProducerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiProducerApplication.class, args);
+  }
+}
---
+++ api-producer/src/main/java/com/example/api/producer/controller/DemoController.java
@@ -0,0 +1,6 @@
+package com.example.api.producer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {}
